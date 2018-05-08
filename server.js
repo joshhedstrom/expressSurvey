4e8401f62f0ff520abdaeb2b8db7183cf3ab1989
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-08 13:14:26 -0500

    wrote file routes

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.get('/', (req, res)=>{
+	res.sendFile(path.join(__dirname, './app/public/index.html'))
+})
+
+app.get('/survey', (req, res)=>{
+	res.sendFile(path.join(__dirname, './app/public/survey.html'))
+})
+
 // require('./app/routing/apiRoutes')(app);
 // require('./app/routing/htmlRoutes')(app);
 
