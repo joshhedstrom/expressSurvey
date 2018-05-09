45acbec363f8a4db5b5d10e256cce7b51b251015
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-09 11:05:40 -0500

    added images, formatted survey elements and questions

---
+++ app/public/app.js
@@ -0,0 +1,14 @@
+let surveyAnswers = [];
+
+$('#submit').click(function(e) {
+	e.preventDefault()
+
+	for (let i = 1; i < 41; i++) {
+		console.log(i)
+		let id = '#question' + i;
+		let questionScore = $(id).val();
+		surveyAnswers.push(questionScore);
+
+	}
+
+});
