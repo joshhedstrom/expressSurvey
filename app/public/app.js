d2a406ff8124d7f1bb285ade10e9094e75120cd4
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-11 14:32:03 -0500

    added title and subtitles to survey, aligned active slider indicator

--- app/public/app.js
+++ app/public/app.js
@@ -13,3 +13,32 @@ $('#submit').click(function(e) {
 	console.log(surveyAnswers)
 
 });
+
+
+// $(document).ready(()=> {
+	// const questionArray = ['testing1', 'testing2', 'testing3'];
+	// let divID = $('#questions');
+
+	// questionArray.forEach((elem, i)=> {
+	// 	console.log(elem, i)
+		
+	// 	$(divID).append().html(`
+	// 		<div class="row hoverable">
+	// 			<form class="center-align ">
+	// 				<h4 class="center-align col s12">${elem}</h4>
+	// 				<p class="col s1 m3 right-align">1</p>
+	// 				<p class="range-field">
+	// 					<input class="col s10 m6" type="range" id="question${i + 1}" min="0" max="10"/>
+	// 				</p>
+	// 				<p class="col s1 m3 left-align">10</p>
+	// 			</form>
+	// 		</div>`)
+	// 	// console.log(item)
+	// 	console.log(divID)
+
+		
+
+	// $(divID).append(item);
+
+// });
+	// });
