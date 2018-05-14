ba8ee26f77572a7cadb1af0d77ccb02c36b857a5
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-14 17:21:40 -0500

    cleaned up code

--- app/data/survey.js
+++ app/data/survey.js
@@ -3,60 +3,52 @@ const otterPersonality = [0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0,
 const retrieverPersonality = [0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0];
 const beaverPersonality = [0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 10];
 
-const testPersonality = [4, 5, 8, 2, 9, 3, 5, 8, 3, 3, 3, 5, 8, 2, 9, 4, 5, 8, 2, 9, 4, 5, 3, 8, 3, 0, 9, 4, 5, 8, 2, 3, 4, 5, 8, 2, 9, 4, 5, 8]
+// const testPersonality = [4, 5, 8, 2, 9, 3, 5, 8, 3, 3, 3, 5, 8, 2, 9, 4, 5, 8, 2, 9, 4, 5, 3, 8, 3, 0, 9, 4, 5, 8, 2, 3, 4, 5, 8, 2, 9, 4, 5, 8]
+// let me = [3, 9, 7, 7, 9, 10, 9, 8, 7, 10, 7, 8, 8, 6, 2, 6, 10, 3, 0, 6, 10, 9, 0, 2, 6, 6, 7, 6, 3, 6, 8, 3, 2, 7, 4, 3, 8, 3, 1, 10]
 
-let lionScore = 0;
-let otterScore = 0;
-let retrieverScore = 0;
-let beaverScore = 0;
+module.exports = function testLoop(array) {
+    let lionScore = 0;
+    let otterScore = 0;
+    let retrieverScore = 0;
+    let beaverScore = 0;
 
+    let lionPercent;
+    let otterPercent;
+    let retrieverPercent;
+    let beaverPercent;
 
-
-let me = [3, 9, 7, 7, 9, 10, 9, 8, 7, 10, 7, 8, 8, 6, 2, 6, 10, 3, 0, 6, 10, 9, 0, 2, 6, 6, 7, 6, 3, 6, 8, 3, 2, 7, 4, 3, 8, 3, 1, 10]
-
-function testLoop() {
-
-    // if (i < 0) {
-    // 	i = i * -1;
-    // }
-
-    for (let i = 0; i < me.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         let placeholder = i % 4
-        // console.log(i, ': ', placeholder, '|| ', me[i])
 
         switch (placeholder) {
             case 0:
-                lionScore += me[i];
-                console.log('new lion score: ', lionScore)
+                lionScore += array[i];
                 break;
             case 1:
-                otterScore += me[i];
-                console.log('new otter score: ', otterScore)
+                otterScore += array[i];
                 break;
             case 2:
-                retrieverScore += me[i];
-                console.log('new retriever score: ', retrieverScore)
+                retrieverScore += array[i];
                 break;
             case 3:
-                beaverScore += me[i];
-                console.log('new beaver score: ', beaverScore)
+                beaverScore += array[i];
                 break;
         }
     }
 
+    function analyzeResults() {
+        let total = otterScore + beaverScore + lionScore + retrieverScore;
 
-    // for (let i = 0; i <= 40; i+4) {
-    // 	console.log('1: ', i);
-    // }
-    // for (let i = 1; i <= 40; i + 4) {
-    // 	console.log('2: ', i);
-    // }
-    // for (let i = 2; i <= 40; i + 4) {
-    // 	console.log('3: ', i);
-    // }
-    // for (let i = 3; i <= 40; i + 4) {
-    // 	console.log('45: ', i);
-    // }
+        otterPercent = otterScore / total;
+        beaverPercent = beaverScore / total;
+        lionPercent = lionScore / total;
+        retrieverPercent = retrieverScore / total;
+
+        console.log('otter: ', otterPercent);
+        console.log('beaver: ', beaverPercent);
+        console.log('lion: ', lionPercent);
+        console.log('retriever: ', retrieverPercent);
+
+    }
+    analyzeResults();
 }
-
-testLoop();
--- server.js
+++ server.js
@@ -8,16 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-	res.sendFile(path.join(__dirname, './app/public/index.html'))
-})
-
-app.get('/survey', (req, res)=>{
-	res.sendFile(path.join(__dirname, './app/public/survey.html'))
-})
-
-// require('./app/routing/apiRoutes')(app);
-// require('./app/routing/htmlRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
 
 app.listen(PORT, ()=>{
 	console.log(`Listening on PORT: ${PORT}`);
